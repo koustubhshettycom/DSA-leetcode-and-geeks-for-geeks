e01dbcdc965bec42a56150e2f49d23925809af5e
koustubh shetty <koustubhd2007@gmail.com>
2025-08-24 18:37:40 +0530

    Time: 1 ms (79.82%), Space: 41.9 MB (18.22%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         char[] arr = sentence.toCharArray();
+        // adding if its true or not 
         boolean[] arr2 = new boolean[26];
         for(char ch : arr){
            arr2[ch - 'a'] = true;
         }
+        // checking each and every element
         for(boolean boo:arr2){
             if(!boo){
                  return false;
@@ -14,3 +16,4 @@ class Solution {
         return true;
     }
 }
+// Time complexity is great and is of O(n) 8/10
