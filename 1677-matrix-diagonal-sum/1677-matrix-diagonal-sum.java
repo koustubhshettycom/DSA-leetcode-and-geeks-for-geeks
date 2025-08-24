787e762d81a013d9198920751277ae7620b999ee
koustubh shetty <koustubhd2007@gmail.com>
2025-08-24 17:21:02 +0530

    Time: 1 ms (28.32%), Space: 44.4 MB (60.08%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
         int sum =0;
+        //primary diagonal
         for(int i=0;i<mat.length;i++){
           sum+= mat[i][i]; 
         }
 
 
 
-
+        // secondary diagonal
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[i].length;j++)
                 if(i+j==mat.length-1 && i!=j){
@@ -19,3 +20,5 @@ class Solution {
         
     }
 }
+// time complexity is not that good O(n*n) 6/10 
+
