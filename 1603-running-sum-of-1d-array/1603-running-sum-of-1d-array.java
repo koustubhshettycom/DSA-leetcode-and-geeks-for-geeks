4e746997e482601657c81c5f6dc6bf5a91be14e9
koustubh shetty <koustubhd2007@gmail.com>
2025-08-24 17:03:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (24.27%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] runningSum(int[] nums) {
         int prev = 0;
         int[] arr = new int[nums.length];
+        // having a previous and adding by treversing 
         for(int i=0;i<nums.length;i++){
             prev+= nums[i];
             arr[i]= prev;
@@ -9,3 +10,5 @@ class Solution {
         return arr;
     }
 }
+
+// time complexity is great O(n) 9/10
