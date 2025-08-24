1dae68b78a19534ad153cea5a10e96ac530bdb86
koustubh shetty <koustubhd2007@gmail.com>
2025-08-24 16:59:56 +0530

    Time: 0 ms (100%), Space: 44.8 MB (41.31%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
+        //extending the length of the array to 
         int[] arr = new int[2*n];
         for(int i = 0, j = 0;i < arr.length && j < n;i += 2,j++){
-            
+        // adding the elements 
                 arr[i]=nums[j];
                 arr[i+1]=nums[j+n];
 
@@ -17,4 +18,5 @@ class Solution {
        
        
     }
+
 
