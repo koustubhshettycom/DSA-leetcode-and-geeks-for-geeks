f2b07b9dc59f0b56b83315842e04c9e569a29950
koustubh shetty <koustubhd2007@gmail.com>
2025-08-24 18:40:48 +0530

    Time: 1 ms (99.09%), Space: 45.3 MB (81.94%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,10 +1,13 @@
 class Solution {
     public int[] buildArray(int[] nums) {
         int size = nums.length;
+        //creating new array for convinece
         int[] arr = new int[size];
+        // traversing and adding each and eevry element
         for(int i=0;i<nums.length;i++){
             arr[i] = nums[nums[i]];
         } 
         return arr;
     }
 }
+// Time complexity is great and is O(n) 9/10
