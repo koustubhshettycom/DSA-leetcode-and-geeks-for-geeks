ef82a86d706cc545475c159be31d7a4ddf5feabc
koustubh shetty <koustubhd2007@gmail.com>
2025-08-26 03:30:10 +0530

    Time: 0 ms (100%), Space: 41.9 MB (8.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -7,19 +7,12 @@ class Solution {
         List<Integer> ll = new ArrayList<>();
         spiral(arr, 0, arr.length - 1, 0, arr[0].length - 1, ll);
         return ll;
-
 // used recursion and while loops if condition for last two to check the row and col
-
-
-
-
     }
-    
         public void spiral(int[][] arr, int startrow,int endrow,int startcol,int endcol,List<Integer> ll){
           // end condition needed to check if it overlaps the matrix again
             if(startrow > endrow || startcol > endcol ){
-                return ;
-                
+                return ;   
             }
             int row=startrow;
             int col=startcol;
@@ -54,14 +47,9 @@ class Solution {
                   row--;
                     }
                 }
-                
-
-            
+   
                // using recursion to for the inner loops 
                 spiral( arr, startrow+1, endrow-1, startcol+1, endcol-1,ll);
-            }
+            } // Time complexity is great and good job at your first medium question O(m*n) 10/10
 
-            
-        
-        
 }
