4b4162b47a86e8bd597eb4c17abfe287ec978430
koustubh shetty <koustubhd2007@gmail.com>
2025-08-28 12:14:39 +0530

    Time: 15 ms (18.3%), Space: 56.5 MB (90.07%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+     
+     int pointer=1;   
+     Arrays.sort(arr);
+     for(int i=0;i<arr.length;i++){
+        if(arr[i]== pointer){
+            pointer++;
+        }
+     }
+        return pointer;
+
+
+
+    }
+}
