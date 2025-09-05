85ff5221cb1a645584625f1256c520c754cd7a0b
koustubh shetty <koustubhd2007@gmail.com>
2025-09-05 13:27:37 +0530

    Time: 3 ms (48.95%), Space: 43.3 MB (75.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        
+        //sort both the arrays first
         Arrays.sort(nums1);
         Arrays.sort(nums2);
+        // create a ll to add common elements 
         LinkedList<Integer> ll= new LinkedList<>();
         int i=0;
         int j=0;
+        // run a loop until its end
         while(i<nums1.length&& j<nums2.length){
             if(nums1[i]==nums2[j]){
             if(ll.isEmpty() || ll.getLast()!=nums1[i]){
                 ll.add(nums1[i]);
-                
             }
             i++;
             j++;
@@ -24,7 +25,7 @@ class Solution {
         }
 
         }
-        
+        // replicate then it into array as it is what they have asked 
 
         int[] arr = new int[ll.size()];
         for(int k=0;k<arr.length;k++){
