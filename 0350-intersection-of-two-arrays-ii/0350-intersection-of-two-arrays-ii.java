65c6067dd312b33abbcd88e544d110e7b8b06fde
koustubh shetty <koustubhd2007@gmail.com>
2025-09-05 13:41:25 +0530

    Time: 3 ms (91.41%), Space: 43.4 MB (58.77%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+        // sort the arrays first then create a new ll
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         LinkedList<Integer> ll =new LinkedList<>();
         int i=0;int j=0;
+        // check for the equal value and add it to the ll
         while(i<nums1.length && j<nums2.length){
             if(nums1[i]==nums2[j]){
                 ll.add(nums1[i]);
@@ -17,10 +19,11 @@ class Solution {
                 j++;
             }
         }
+        //from ll add it back to array now
         int[] arr = new int[ll.size()];
         for(int k=0;k<arr.length;k++){
             arr[k]=ll.get(k);
         }
         return arr;
     }
-}
+}// time complexity is O(logn)
