23dfb93cdf4b5ddbdd1412328458955b3e3b8c73
koustubh shetty <koustubhd2007@gmail.com>
2025-09-06 12:07:02 +0530

    Time: 3 ms (12.09%), Space: 41.9 MB (11.22%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,8 +1,9 @@
 class Solution {
     public int specialArray(int[] arr) {
+        // Sort the arry
        Arrays.sort(arr);
        int no = 0;
-       
+    // count one by one 
        while(no<arr.length+1){
         int count=0;
         for(int i=0;i<arr.length;i++){
@@ -23,3 +24,5 @@ class Solution {
        
     }
 }
+// bad time complexity what u did was brute force which is actually bad 
+// time complexity O(logn)
