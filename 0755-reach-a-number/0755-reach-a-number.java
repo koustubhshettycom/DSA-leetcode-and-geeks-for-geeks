98f619d493c84a81b4c7b5556e9336bf55a1a4fe
koustubh shetty <koustubhd2007@gmail.com>
2025-09-10 15:17:44 +0530

    Time: 1 ms (85.41%), Space: 40.4 MB (70.13%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reachNumber(int target) {
+      int count =0;
+      int sum=0;
+      if(target>0){
+        while(sum<target || (sum-target)%2!=0){
+        count++;
+        sum+=count;
+      } 
+      }
+      else{
+        while(sum>target || (sum-target)%2!=0){
+        count--;
+        sum+=count;
+      } 
+      return -count;
+      }
+      
+      return count;
+    }
+}
