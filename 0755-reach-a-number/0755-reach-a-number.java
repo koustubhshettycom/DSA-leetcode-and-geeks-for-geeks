520ea12cd75a2b2166c1b470e36e34c9ccc182d6
koustubh shetty <koustubhd2007@gmail.com>
2025-09-10 15:23:03 +0530

    Time: 2 ms (53.44%), Space: 40.6 MB (39.97%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.java
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -2,9 +2,7 @@ class Solution {
     public int reachNumber(int target) {
       int count =0;
       int sum=0;
-      // logic was hard to find u have deal it with pen and paper
-      // u can return back after exceeding the number if the difference is even no.
-      // as one turn makes a difference of 2 times of that turn     
+      
       if(target>0){
         while(sum<target || (sum-target)%2!=0){
         count++;
