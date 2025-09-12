4d9cb0c3e4dfb41fc72550db3a81b8a08b76fc62
koustubh shetty <koustubhd2007@gmail.com>
2025-09-12 20:18:19 +0530

    Time: 7 ms (7.55%), Space: 41.8 MB (33.96%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,17 @@
+// the main goal of Strings is to learn different categories of commands
+class Solution {
+    public String defangIPaddr(String str) {
+       //iterate through each and every char then defang the ip address
+        int size = str.length();
+        String str2 ="";
+        for(int i=0;i<size;i++){
+            if(str.charAt(i) == '.'){
+                str2= str2 +"[.]";
+            }
+            else{
+                str2= str2 + str.charAt(i);
+            }
+        }
+        return str2;
+    }
+}// time complexity is O(n)
