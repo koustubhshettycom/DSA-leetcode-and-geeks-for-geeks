7cc6d6cb6c998f803ac73a6c08c07f6a622abd94
koustubh shetty <koustubhd2007@gmail.com>
2025-09-18 20:03:13 +0530

    Time: 2 ms (98.82%), Space: 42.8 MB (90.5%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // logic use to pointers but the thing to learn here is 
+        //Character.toLowercase() and Character.isLetterOrDigit()
+        int left =0;
+        int right = s.length()-1;
+        while(left<right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+             if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right))){
+                return false;
+             }
+             left++;
+             right--;
+
+
+        }
+        return true;
+    }
+}//Time complexity is O(n)
