57be2dda8ae52b88035fd9f3081dd9202011cea8
koustubh shetty <koustubhd2007@gmail.com>
2025-10-29 23:13:16 +0530

    Time: 8 ms (79.99%), Space: 44 MB (70.66%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,4 +1,5 @@
 class Solution {
+    // simple code and easy 
     public boolean isPowerOfThree(int n) {
         if(n<=0){
             return false;
@@ -9,4 +10,4 @@ class Solution {
         }
         return num==1;
     }
-}
+}// Time complexity is O(logn)
