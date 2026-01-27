a66ab3b8bf8032aa66fbd480f97682f10eda7ec9
koustubh shetty <koustubhd2007@gmail.com>
2026-01-28 01:55:20 +0530

    Time: 1 ms (99.85%), Space: 46.9 MB (76.35%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,7 +3,7 @@ class Solution {
         // property of XOR operator is that anything XOR'ed with itself is 0
         int num =0;
         for(int item: nums){
-            num= num^item;
+            num^=item;
         }
         return num;
         
