0e98a802ea0a668cea43e000f3ada70b818f4bbf
koustubh shetty <koustubhd2007@gmail.com>
2026-01-28 01:53:57 +0530

    Time: 1 ms (99.85%), Space: 47.2 MB (11.91%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // property of XOR operator is that anything XOR'ed with itself is 0
+        int num =0;
+        for(int item: nums){
+            num= num^item;
+        }
+        return num;
+        
+    }
+}
+// Time complexity O(n)
