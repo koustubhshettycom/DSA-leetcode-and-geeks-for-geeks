05c59958a2e813ec8c76925a56e670229ce6574c
koustubh shetty <koustubhd2007@gmail.com>
2026-02-11 18:27:47 +0530

    Time: 0 ms (100%), Space: 43 MB (31.19%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -12,6 +12,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         // traversing through the linkedlist first to get the count 
         // second traversal till its half then returning it 
+        // remember this one approach of other solution of fast and slow pointer method 
         ListNode temp = head;
         int count =1;
         
