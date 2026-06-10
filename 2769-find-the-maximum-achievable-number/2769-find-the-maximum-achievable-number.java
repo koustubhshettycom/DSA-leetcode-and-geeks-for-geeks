d2fccbf16a9a0cc61dd107a01ef0331a845f612c
koustubh shetty <koustubhd2007@gmail.com>
2026-06-11 01:51:02 +0530

    Time: 1 ms (100%), Space: 43.5 MB (9.91%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        // easy logic read the question properly and look over the test cases 
+        return num+(2*t);
+        
+    }
+}
+// Time complexity O(1)
