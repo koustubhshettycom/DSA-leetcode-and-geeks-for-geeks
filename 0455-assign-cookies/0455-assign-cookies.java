e250f89ce00393942324bd4766a2ec73ae1f1811
koustubh shetty <koustubhd2007@gmail.com>
2026-06-12 16:21:41 +0530

    Time: 16 ms (10.57%), Space: 51.6 MB (36.01%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        //Greedy method when 2 arrays are given
+        // sort them choose the best possible answer for the exisiting one
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start1=0;
+        int start2=0;
+        int count=0;
+        while(start1<g.length && start2<s.length){
+            if(g[start1]<=s[start2]){
+                count++;
+                start1++;
+                start2++;
+
+            }
+            else{
+                start2++;
+            }
+        }
+        return count;
+        
+    }
+}// Time complexity is O(n)
