a76f29441485a0b58b1d3d18413050c10e0ebfab
koustubh shetty <koustubhd2007@gmail.com>
2026-06-21 15:06:19 +0530

    Time: 17 ms (56.72%), Space: 48 MB (26.06%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxDistance(String moves) {
+        // easy logic track x and y axis ans add the spaces in the end 
+        char[] arr = moves.toCharArray();
+        int space =0;
+        int x=0;
+        int y=0;
+
+        for(char ch: arr ){
+            if(ch=='U'){
+                y++;
+            }
+            else if(ch=='D'){
+                y--;
+            }
+            else if(ch=='L'){
+                x--;
+            }
+            else if(ch=='R'){
+                x++;
+            }
+            else{
+                space++;
+            } 
+        }
+        return Math.abs(x)+Math.abs(y)+Math.abs(space);
+
+        
+    }
+}//Time complexity is O(n)
