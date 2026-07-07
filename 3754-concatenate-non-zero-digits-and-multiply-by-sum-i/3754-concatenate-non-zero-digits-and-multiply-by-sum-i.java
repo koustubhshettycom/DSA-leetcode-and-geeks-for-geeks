e5f5b7e251cf00e93a1544d77a99ae176540ca27
koustubh shetty <koustubhd2007@gmail.com>
2026-07-08 01:08:46 +0530

    Time: 1 ms (99.85%), Space: 42.7 MB (35.05%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        //easy logic go acc to question eliminate the number 0 
+        //keep adding to sum and store the num return it
+        int num = n;
+        long sum=0;
+        long curr=0;
+        int exp =0;
+        while(num>0){
+            
+            int ans = num%10;
+            if(ans!=0){
+            sum+= ans;
+            
+            curr+=ans*Math.pow(10,exp);
+            exp++;
+            }
+            num= num/10;
+           
+
+        }
+
+        return curr*sum;
+        
+    }
+}// Time complexity is O(n)
