e75d040fa12e1a45af8ed4eec76d37d35e80f0b5
koustubh shetty <koustubhd2007@gmail.com>
2026-09-02 15:49:02 +0530

    Time: 0 ms (100%), Space: 45.3 MB (16.73%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Easy logic for all the cases ie all even or all odd one even or odd
+        return true;//This will always be valid 
+    }
+}//Time complexity is O(1)
