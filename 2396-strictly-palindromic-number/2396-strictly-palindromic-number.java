a1420634371c35f84d50994589e20afb0f96bb0a
koustubh shetty <koustubhd2007@gmail.com>
2026-09-15 18:08:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.94%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        //It can never exist as per the condition
+        return false;
+
+        
+    }
+}//Time complexity is O(1)
